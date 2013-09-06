a4340b8675be471cd56e905151ef666b8179927b
Sharma Elanthiraiyan <sharma@inxsasia.com>
2013-09-06 12:48:47 +0530

    Initial commit.

---
+++ SimpleCharts/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleCharts
+//
+//  Created by Sharma Elanthiraiyan on 9/6/13.
+//  Copyright (c) 2013 Sharma Elanthiraiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SimpleCharts/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleCharts
+//
+//  Created by Sharma Elanthiraiyan on 9/6/13.
+//  Copyright (c) 2013 Sharma Elanthiraiyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
